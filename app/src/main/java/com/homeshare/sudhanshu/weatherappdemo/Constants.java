b1fc9fb0ca49b511b70f2f015dac10d176647c38
Sudhanshu Siddh <sudhanshu.15@gmail.com>
2017-09-26 19:09:46 -0400

    Completed Fragment layout

---
+++ app/src/main/java/com/homeshare/sudhanshu/weatherappdemo/Constants.java
@@ -0,0 +1,11 @@
+package com.homeshare.sudhanshu.weatherappdemo;
+
+/**
+ * Created by sudhanshu on 9/26/17.
+ */
+
+public class Constants {
+
+    public static final String API_KEY = "17e3cc1b98b51a8d6a446ffc133c43f1";
+
+}
---
+++ app/src/main/java/com/homeshare/sudhanshu/weatherappdemo/GeoLoaction.java
@@ -0,0 +1,11 @@
+package com.homeshare.sudhanshu.weatherappdemo;
+
+/**
+ * Created by sudhanshu on 9/26/17.
+ */
+
+public class GeoLoaction {
+
+
+
+}
